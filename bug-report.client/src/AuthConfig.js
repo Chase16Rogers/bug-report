d87ac7308675ccf6e618daffd9bb8f59e03e5e0d
Chase16Rogers <chase16rogers@gmail.com>
2021-03-10 09:26:50 -0700

    deploy 2

--- bug-report.client/src/AuthConfig.js
+++ bug-report.client/src/AuthConfig.js
@@ -1,3 +1,3 @@
 export const domain = 'dev-c3yjh80u.us.auth0.com'
 export const clientId = 'ESvi5uJzE0eVk5hMVjYE2dKiP2yz09Dk'
-export const audience = 'https://partner-trello'
+export const audience = 'https://bug-report'
